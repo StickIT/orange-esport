b32636dc23b8da376e3c5cc941f9c4bd23c25727
Florian <florian@florians-mac-pro-1.home>
2016-04-11 18:16:51 +0200

    Add start sequences

--- www/dev/js/app.js
+++ www/dev/js/app.js
@@ -34,20 +34,150 @@ require.config({
         'typed': 'typed'
     }
 });
-require(['jquery', 'bootstrap', 'velocity', 'velocity-ui', 'parallax', 'typed'], function(jQuery) {
+require(['jquery', 'bootstrap', 'velocity', 'velocity-ui', 'parallax', 'typed'], function(jQuery, Velocity) {
     jQuery(function() {
         jQuery(document).ready(function() {
 			console.log('It works !');
+            // Open modal at start
             jQuery('#modal-registration-steps').modal('show');
-            jQuery('.parallax-container-steps').parallax();
-            jQuery('.speech-photo-left img').velocity("transition.slideLeftBigIn", { duration: 1000 });
-            jQuery('.speech-text-left .speech-text-left-logo').velocity("transition.slideRightBigIn", { duration: 1000 });
-            jQuery('.speech-text-left .speech-text-left-bubble').velocity({ opacity: [1, 0], width: ['550px', '0px'] }, { duration: 800, delay: 400, easeing: 'ease-out' });
-            jQuery('.speech-text-left, .speech-photo-left').velocity("transition.fadeIn", { duration: 1000, stagger: 200});
-            jQuery(".text").typed({ 
-                strings: ["<b>Salut ! Moi c'est Rush</b> <br> Et toi t'es qui ?"],
-                startDelay: 1000
-            });
+            // Steps
+            registrationStep00();
 		});
+        // Registration Sequences        
+        // Step 00 : Welcome
+        function registrationStep00(){
+            // Load Parallax
+            jQuery('.parallax-step-00').parallax();
+            // Animate speechbubble
+            jQuery('.parallax-step-00 .speech-text-left, .parallax-step-00 .speech-photo-left').velocity({opacity: [1, 0]}, { duration: 100 });
+            // jQuery('.parallax-step-00 .speech-photo-left img').velocity({opacity: [1, 0], translateY: ['0', '-150%']}, { duration: 500, easing: 'ease-out' }); 
+            jQuery('.parallax-step-00 .speech-text-left-logo').velocity({width: ['150px', '0']}).velocity({left: ['0', '50%'], bottom: ['0', '50%']}, { 
+                duration: 500,
+                easing: 'ease-out',
+                complete: function(){
+                    jQuery('.parallax-step-00 .speech-text-left-logo').velocity('callout.pulse', {duration: 300, delay: 200});
+                    jQuery('.parallax-step-00 .speech-text-left .speech-text-left-bubble').velocity({ opacity: [1, 0], width: ['550px', '0px'] }, { 
+                        duration: 500,
+                        easing: 'ease-out',
+                        complete: function(){                                    
+                            jQuery('.parallax-step-00 .text').typed({ 
+                                strings: ["Salut ! Moi c'est Rush. <br> <b>Et toi t'es qui ?</b>"],
+                                showCursor: false,
+                                callback: function() { 
+                                    jQuery('.parallax-step-00 .text').append('<input class="typedInput" type="text" name="pseudo" autofocus>');                                    
+                                    jQuery('.parallax-step-00 .speech-text-left-bubble a').click(function(e){
+                                        e.preventDefault();
+                                        e.stopPropagation();  
+                                        if(jQuery('input[name="pseudo"]').val() != ''){
+                                            pseudoUser = jQuery('input[name="pseudo"]').val();
+                                            jQuery('.parallax-step-00 .speech-text-left, .parallax-step-00 .speech-photo-left').velocity({opacity: [0, 1]}, { duration: 500, display: "none" });
+                                            jQuery('.parallax-step-00 .speech-photo-left img').velocity({opacity: [0, 1], translateY: ['-150%', '0']}, { duration: 500, display: "none" }); 
+                                            jQuery('.parallax-step-00 .speech-text-left-logo, .parallax-step-00 .speech-text-left-bubble').velocity({opacity: [0, 1], translateY: ['150%', '0']}, {
+                                                duration: 500, 
+                                                display: 'none',
+                                                complete: function(){registrationStep02();}
+                                            }); 
+
+                                        }else{
+                                            jQuery('.parallax-step-00 .speech-text-left .speech-text-left-bubble').velocity('callout.shake');
+                                            jQuery('input[name="pseudo"]').addClass('has-error');
+                                        }
+                                    });  
+                                }
+                            });
+                        }
+                    });
+                }
+            });       
+        }
+        // Step 01 : Qui es-tu ?
+        function registrationStep01(){
+            // Load Parallax
+            jQuery('.parallax-step-01').parallax();
+            // Animate speechbubble
+            jQuery('.parallax-step-01 .speech-text-left, .parallax-step-01 .speech-photo-left').velocity({opacity: [1, 0]}, { duration: 100 });
+            jQuery('.parallax-step-01 .speech-photo-left img').velocity({opacity: [1, 0], translateY: ['0', '-150%']}, { duration: 500, easing: 'ease-out' }); 
+            jQuery('.parallax-step-01 .speech-text-left-logo').velocity('transition.bounceUpIn', { 
+                duration: 500,
+                easing: 'ease-out',
+                complete: function(){
+                    jQuery('.parallax-step-01 .speech-text-left-logo').velocity('callout.pulse', {duration: 300, delay: 200});
+                    jQuery('.parallax-step-01 .speech-text-left .speech-text-left-bubble').velocity({ opacity: [1, 0], width: ['550px', '0px'] }, { 
+                        duration: 500,
+                        easing: 'ease-out',
+                        complete: function(){                                    
+                            jQuery('.parallax-step-01 .text').typed({ 
+                                strings: ["Salut ! Moi c'est Rush. <br> <b>Et toi t'es qui ?</b>"],
+                                showCursor: false,
+                                callback: function() { 
+                                    jQuery('.parallax-step-01 .text').append('<input class="typedInput" type="text" name="pseudo" autofocus>');                                    
+                                    jQuery('.parallax-step-01 .speech-text-left-bubble a').click(function(e){
+                                        e.preventDefault();
+                                        e.stopPropagation();  
+                                        if(jQuery('input[name="pseudo"]').val() != ''){
+                                            pseudoUser = jQuery('input[name="pseudo"]').val();
+                                            jQuery('.parallax-step-01 .speech-text-left, .parallax-step-01 .speech-photo-left').velocity({opacity: [0, 1]}, { duration: 500, display: "none" });
+                                            jQuery('.parallax-step-01 .speech-photo-left img').velocity({opacity: [0, 1], translateY: ['-150%', '0']}, { duration: 500, display: "none" }); 
+                                            jQuery('.parallax-step-01 .speech-text-left-logo, .parallax-step-01 .speech-text-left-bubble').velocity({opacity: [0, 1], translateY: ['150%', '0']}, {
+                                                duration: 500, 
+                                                display: 'none',
+                                                complete: function(){registrationStep02();}
+                                            }); 
+
+                                        }else{
+                                            jQuery('.parallax-step-01 .speech-text-left .speech-text-left-bubble').velocity('callout.shake');
+                                            jQuery('input[name="pseudo"]').addClass('has-error');
+                                        }
+                                    });  
+                                }
+                            });
+                        }
+                    });
+                }
+            });       
+        }
+        // Step 02 : Bravo, tu fais parti du club !
+        function registrationStep02(){
+            // Load parallax
+            jQuery('.parallax-step-02').parallax();
+            // Remove class
+            jQuery('.parallax-step-02').removeClass('hidden');
+            jQuery('.parallax-step-02 .text-pseudoUser').html(pseudoUser);
+            // Animate speechbubble
+            jQuery('.parallax-step-02 .speech-text-left, .parallax-step-02 .speech-photo-left').velocity({opacity: [1, 0]}, { duration: 100 });
+            jQuery('.parallax-step-02 .speech-photo-left img').velocity({opacity: [1, 0], translateX: ['0', '-150%']}, { duration: 500, easing: 'ease-out' }); 
+            jQuery('.parallax-step-02 .speech-text-left-logo').velocity({opacity: [1, 0], translateY: ['0', '150%']}, { 
+                duration: 500,
+                easing: 'ease-out',
+                complete: function(){
+                    jQuery('.parallax-step-02 .speech-text-left .speech-text-left-bubble').velocity({ opacity: [1, 0], width: ['550px', '0px'] }, { 
+                        duration: 500,
+                        easing: 'ease-out',
+                        complete: function(){                                    
+                            jQuery('.parallax-step-02 .text').typed({ 
+                                strings: ["Te voilà parmi nous !<br> <b>Es-tu prêt à découvrir l'expérience ?</b>"],
+                                showCursor: false,
+                                callback: function() {                                  
+                                    jQuery('.parallax-step-02 .speech-text-left-bubble a').click(function(e){
+                                        e.preventDefault();
+                                        e.stopPropagation();  
+                                        if(jQuery('input[name="pseudo"]').val() != ''){
+                                            var pseudoUser = jQuery('input[name="pseudo"]').val();
+                                            jQuery('.parallax-step-02 .speech-text-left, .parallax-step-02 .speech-photo-left').velocity({opacity: [0, 1]}, { duration: 1000, display: "none" });
+                                            jQuery('.parallax-step-02 .speech-photo-left img').velocity({opacity: [0, 1], translateX: ['-150%', '0']}, { duration: 500, display: "none" }); 
+                                            jQuery('.parallax-step-02 .speech-text-left-logo, .parallax-step-02 .speech-text-left-bubble').velocity({opacity: [0, 1], translateX: ['150%', '0']}, { duration: 500, display: 'none' }); 
+
+                                        }else{
+                                            jQuery('.parallax-step-02 .speech-text-left .speech-text-left-bubble').velocity('callout.shake');
+                                            jQuery('input[name="pseudo"]').addClass('has-error');
+                                        }
+                                    });  
+                                }
+                            });
+                        }
+                    });
+                }
+            });       
+        }
     });
 });
